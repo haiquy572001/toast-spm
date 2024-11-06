5e49c0dd4502a758839838b56948d48298c98c25
Lê Hải Quý <haiquy2001@gmail.com>
2024-11-06 15:01:19 +0700

    fix

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version: 5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.4
 import PackageDescription
 
 let package = Package(
@@ -9,18 +7,22 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Toast",
             targets: ["Toast"]),
     ],
+    dependencies: [
+        // This is where you add external dependencies.
+        .package(url: "https://github.com/haiquy572001/toast-spm.git", from: "1.0.0")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Toast",
+            dependencies: [
+                // Include the external dependency here if needed.
+                .product(name: "toast-spm", package: "toast-spm")
+            ],
             path: "Sources"
         ),
-
     ]
 )
